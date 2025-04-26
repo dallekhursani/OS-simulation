f12e6025e9945ede6c0e6536604b0bd0c490a2bd
The Nirajan.xyz <166149014+dallekhursani@users.noreply.github.com>
2025-04-26 17:27:30 +0545

    Create file_system.h

---
+++ file_system.h
@@ -0,0 +1,13 @@
+#ifndef FILE_SYSTEM_H
+#define FILE_SYSTEM_H
+
+class FileSystem {
+public:
+    void menu();
+private:
+    void createFile();
+    void readFile();
+    void deleteFile();
+};
+
+#endif
